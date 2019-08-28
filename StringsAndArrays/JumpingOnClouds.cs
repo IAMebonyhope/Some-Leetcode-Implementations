6bf5c09fc79f60325dc3b522d361806c938c8fd5
ebonyhope <blessyn2hope@gmail.com>
2019-08-28 04:06:03 +0100

    hackerrank:easy:repeatedString

--- StringsAndArrays/JumpingOnClouds.cs
+++ StringsAndArrays/JumpingOnClouds.cs
@@ -6,7 +6,43 @@ using System.Threading.Tasks;
 
 namespace StringsAndArrays
 {
+    //Site: Hackerrank
+    //Category: Warm Up Interview Preparation Kit
+    //Difficulty: Easy
     class JumpingOnClouds
     {
+        public static int jumpingOnClouds(int[] c)
+        {
+
+            int jumps = 0;
+            int count = 0;
+
+            if (c.Length == 0)
+            {
+                return 0;
+            }
+
+            while (count < (c.Length - 1))
+            {
+
+                if (((count + 2) >= c.Length) && (c[count + 1] != 0))
+                {
+                    break;
+                }
+                else if (((count + 2) < c.Length) && (c[count + 2] == 0))
+                {
+                    count += 2;
+                }
+                else
+                {
+                    count += 1;
+                }
+
+                jumps += 1;
+            }
+
+            return jumps;
+
+        }
     }
 }
---
+++ StringsAndArrays/RepeatedString.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsAndArrays
+{
+    //Site: Hackerrank
+    //Category: Warm Up Interview Preparation Kit
+    //Difficulty: Easy
+    class RepeatedString
+    {
+        static long repeatedString(string s, long n)
+        {
+
+            long noA = countNumberOfAs(s, s.Length);
+
+            noA = (n / s.Length) * noA;
+
+            long repeatedS = (n / s.Length) * s.Length;
+
+            int remainingFirstN = (int)(n - repeatedS);
+
+            noA += countNumberOfAs(s, remainingFirstN);
+
+            return noA;
+        }
+
+        static int countNumberOfAs(string s, int n)
+        {
+            int noA = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (s[i] == 'a')
+                {
+                    noA += 1;
+                }
+            }
+
+            return noA;
+        }
+
+    }
+}
