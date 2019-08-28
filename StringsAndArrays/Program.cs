475f4cc1b03480b4f4faa1fbac75b79e0ab1f7d9
ebonyhope <blessyn2hope@gmail.com>
2019-08-28 03:56:06 +0100

    hackerrank:easy:jumping on the cloud

--- StringsAndArrays/Program.cs
+++ StringsAndArrays/Program.cs
@@ -10,6 +10,16 @@ namespace StringsAndArrays
     {
         static void Main(string[] args)
         {
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] c = Array.ConvertAll(Console.ReadLine().Split(' '), cTemp => Convert.ToInt32(cTemp));
+
+            int result = 0;
+
+            Console.WriteLine(result);
+
+            Console.ReadLine();
         }
     }
 }
