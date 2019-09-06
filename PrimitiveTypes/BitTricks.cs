5e674bb70fb4d458c2a25e479fe068e288b3b9c0
ebonyhope <blessyn2hope@gmail.com>
2019-09-06 01:59:19 +0100

    added some bit hacks

---
+++ PrimitiveTypes/BitTricks.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimitiveTypes
+{
+    class BitTricks
+    {
+        //count number of bits that are set to 1
+        public int countBits(int x)
+        {
+            int noOfBits = 0;
+            uint h = (uint)x;
+
+            while (h != 0)
+            {
+                noOfBits += (int)(h & 1);
+                h >>= 1;
+            }
+
+            return noOfBits;
+        }
+    }
+}
