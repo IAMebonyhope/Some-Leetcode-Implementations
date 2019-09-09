86d744eb7fc86c3dc678d8a115dbe84312f63a3d
ebonyhope <blessyn2hope@gmail.com>
2019-09-09 05:46:42 +0100

    solved some questions on leetcode

---
+++ PrimitiveTypes/HappyNumbers.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimitiveTypes
+{
+    class HappyNumbers
+    {
+        //Site: LeetCode
+        //Category: Problems
+        //Difficulty: Easy
+        public bool IsHappy(int n)
+        {
+            int slow = n;
+            int fast = n;
+
+            while ((slow != 1) && (fast != 1))
+            {
+                slow = sumOfSquares(slow);
+                fast = sumOfSquares(sumOfSquares(fast));
+
+                if ((slow != 1) && (fast != 1) && (slow == fast))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+
+        private int sumOfSquares(int n)
+        {
+
+            int result = 0;
+
+            while (n > 0)
+            {
+                int remainder = n % 10;
+                result += (remainder * remainder);
+                n = n / 10;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ StringsAndArrays/PermutatingTwoArrays.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsAndArrays
+{
+    //Site: Hackerrank
+    //Category: Problems
+    //Difficulty: Easy
+    class PermutatingTwoArrays
+    {
+        static string twoArrays(int k, int[] A, int[] B)
+        {
+
+            Array.Sort(A);
+            Array.Sort(B);
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                int x = A[i];
+                int y = B[B.Length - 1 - i];
+                if ((x + y) < k)
+                {
+                    return "NO";
+                }
+            }
+
+
+            return "YES";
+        }
+    }
+}
