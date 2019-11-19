5266fb3c1197dfbbb0f20c3a4b533cfea84feb43
ebonyhope <blessyn2hope@gmail.com>
2019-11-19 03:49:19 +0100

    solved 3 sum

---
+++ Python Implementation/StringsAndArrays/ThreeSum.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 18 10:39:36 2019
+
+@author: HP
+"""
+
+class Solution(object):
+    
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        arrs = []
+        
+        for i in range(len(nums)):
+            
+            if(nums[i] > 0):
+                break
+                
+            elif((i > 0) and (nums[i] == nums[i-1])):
+                continue
+                
+            else:
+                left = i+1
+                right = len(nums) - 1
+                
+                while(left < right):
+                    
+                    if((nums[left] + nums[right]) == -(nums[i])):
+                        
+                        arrs.append([nums[i], nums[left], nums[right]])
+                        
+                        while((left < right) and (nums[left] == nums[left+1])):
+                            left = left+1
+                        left = left+1
+                        
+                        while((left < right) and (nums[right] == nums[right-1])):
+                            right = right - 1
+                        right = right - 1
+                            
+                    elif((nums[left] + nums[right]) > -(nums[i])):
+                        right = right - 1
+                    
+                    else:
+                        left = left+1
+        return arrs
+                            
