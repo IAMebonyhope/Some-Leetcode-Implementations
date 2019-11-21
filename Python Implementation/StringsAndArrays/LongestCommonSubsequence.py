eb02e2797d2997adb1b14888ea366f576fe5eba8
ebonyhope <blessyn2hope@gmail.com>
2019-11-21 15:56:53 +0100

    solved some questions

---
+++ Python Implementation/StringsAndArrays/LongestCommonSubsequence.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 21 09:55:45 2019
+
+@author: HP
+"""
+
+class Solution(object):
+    
+    def longestCommonSubsequence2(self, text1, text2):
+        """
+        :type text1: str
+        :type text2: str
+        :rtype: int
+        """
+        memo = {}
+        
+        for i in range (len(text1)+1):
+            for j in range(len(text2)+1):
+                memo[(i, j)] = 0
+                
+        for i in range (len(text1)-1, -1, -1):
+            for j in range(len(text2)-1, -1, -1):
+                if(text1[i] == text2[j]):
+                    memo[(i,j)] = 1 + memo[(i+1,j+1)]
+                else:
+                    memo[(i,j)] = max(memo[(i+1,j)], memo[(i,j+1)])
+            
+        return memo[(0,0)]
+        
+    def longestCommonSubsequence(self, text1, text2):
+        """
+        :type text1: str
+        :type text2: str
+        :rtype: int
+        """
+        
+        return self.helper(text1, text2, 0, 0, {})
+    
+    def helper(self, x, y, i, j, memo):
+        
+        if((i == len(x)) or (j == len(y))):
+            return 0
+        
+        if((i,j) in memo):
+            return memo[(i,j)]
+        
+        if(x[i] == y[j]):
+            memo[(i,j)] = 1 + self.helper(x, y, i+1, j+1, memo)
+        
+        else:
+            memo[(i,j)] = max(self.helper(x, y, i+1, j, memo), self.helper(x, y, i, j+1, memo))
+            
+        return memo[(i,j)]
+        
---
+++ Python Implementation/TreesAndGraphs/BoundaryTraversal.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 21 13:40:40 2019
+
+@author: HP
+"""
+
+def printBoundaryView(root):
+    leftTraversal(root)
+    leafTraversal(root)
+    rightTraversal(root.right)
+
+def leftTraversal(root):
+    if(root == None):
+        return
+    
+    if((root.left != None) or (root.right != None)):
+        print(root.data, end =" ")
+    
+    leftTraversal(root.left)
+
+
+def rightTraversal(root):
+    if(root == None):
+        return
+    
+    rightTraversal(root.right)
+    
+    if((root.left != None) or (root.right != None)):
+        print(root.data, end =" ")
+    
+    
+def leafTraversal(root):
+    if(root == None):
+        return
+    
+    if((root.left == None) and (root.right == None)):
+        print(root.data, end =" ")
+    
+    leafTraversal(root.left)
+    leafTraversal(root.right)
+    
